28775ebdc425632c005057469484cc37bec0f708
noxtros <mr.kevinkalus@gmail.com>
2025-03-18 14:27:00 +0100

    fix: fixed property name on Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Intro } from "./Components/Intro/Intro";
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <Intro text="Hello Cook" />
+      <Intro welcomeText="Hello Cook" />
     </div>
   );
 }
