4e6b7bddbcb55b05ab59f08637a3b948943f6744
noxtros <mr.kevinkalus@gmail.com>
2025-03-27 12:03:48 +0100

    removed Slide component and story. Adjusted the styling on ImageSlider component

--- src/app/Components/Slider/ImageSlider.tsx
+++ src/app/Components/Slider/ImageSlider.tsx
@@ -3,32 +3,38 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import { mockImages } from "@/lib/mockImages";
-import { Navigation, Pagination } from "swiper/modules";
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+import Link from "next/link";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Link from "next/link";
+import "swiper/css/effect-fade";
 
 export const ImageSlider = () => {
   return (
     <Swiper
+      loop
+      effect="fade"
       navigation
+      pagination={{ type: "bullets" }}
       className="h-96 rounded-lg w-full relative "
-      modules={[Navigation, Pagination]}
+      modules={[Navigation, Pagination, EffectFade, Autoplay]}
+      autoplay
     >
       {mockImages.map((image) => {
         return (
           <SwiperSlide key={image.link}>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              className="block h-full w-full object-cover relative"
+            />
             <Link
               href={image.link}
-              className="flex h-full w-full items-center justify-center"
+              className="absolute bottom-8 right-[150px]  bg-black rounded-2xl p-3  hover:bg-red-500"
             >
-              <Image
-                src={image.src}
-                alt={image.alt}
-                className="block h-full w-full object-cover"
-              />
+              Mehr erfahren
             </Link>
           </SwiperSlide>
         );
